f2d5fd836873df8669b5ea6d012ddfce15abac74
tejas0105 <tspathak0105@gmail.com>
2024-01-08 17:08:55 +0530

    view-count-pending

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,12 +1,66 @@
-import { useContext, useEffect } from "react";
+import { useContext, useEffect, useState } from "react";
 import UserContext from "./context/UserContext";
 
 const Content = () => {
   const { videoList } = useContext(UserContext);
+  const [date, setDate] = useState([]);
+  const [formattedDate, setFormattedDate] = useState([]);
+
+  // useEffect(() => {
+  //   if (videoList) console.log(videoList);
+  // }, [videoList]);
 
   useEffect(() => {
-    if (videoList) console.log(videoList);
+    setDate(
+      videoList &&
+        videoList.length > 0 &&
+        videoList.map((item) => {
+          return item?.snippet?.publishedAt.split("T")[0];
+        })
+    );
   }, [videoList]);
+
+  useEffect(() => {
+    function convertDateFormat(intputDate) {
+      const parts = intputDate.split("-");
+      const year = parseInt(parts[0]);
+      const month = parseInt(parts[1]);
+      const day = parseInt(parts[2]);
+
+      const dateObject = new Date(year, month, day);
+
+      const monthNames = [
+        "Jan",
+        "Feb",
+        "Mar",
+        "Apr",
+        "May",
+        "Jun",
+        "Jul",
+        "Aug",
+        "Sept",
+        "Oct",
+        "Nov",
+        "Dec",
+      ];
+      const monthName = monthNames[dateObject.getMonth()];
+      const dayOfMonth = dateObject.getDate();
+      const yearValue = dateObject.getFullYear();
+
+      const formattedDate = `${monthName}, ${dayOfMonth} ${yearValue}`;
+      return formattedDate;
+    }
+    if (date && date.length > 0) {
+      const formattedDates = date.map((inputDate) => {
+        return convertDateFormat(inputDate);
+      });
+      setFormattedDate(formattedDates);
+    }
+  }, [date]);
+
+  // useEffect(() => {
+  //   console.log(formattedDate);
+  // }, [formattedDate]);
 
   return (
     <div className="content-page-div">
@@ -15,45 +69,72 @@ const Content = () => {
       </div>
       <div className="content-page-info">
         <div className="vid-info-div">
-          {videoList && videoList.length > 0 ? (
-            videoList.map((item) => {
-              const vidThumbnail = item?.snippet?.thumbnails?.default?.url;
-              // console.log(vidThumbnail);
-              return (
-                <div key={item?.id} className="vid-info">
-                  <div className="thumbnail">
-                    <img
-                      src={vidThumbnail}
-                      alt="thumbnail"
-                      style={{
-                        height: `${item?.snippet?.thumbnails?.default?.height}px`,
-                        width: `${item?.snippet?.thumbnails?.default?.width}px`,
-                      }}
-                    />
-                    <div className="title-descript">
-                      <a
-                        target="_blank"
-                        rel="noreferrer"
-                        className="vid-title"
-                        href={`https://youtu.be/${item?.contentDetails?.videoId}`}
-                      >
-                        <p>{item?.snippet?.title}</p>
-                      </a>
-                      {item?.snippet?.description !== "" ? (
-                        <p className="description">
-                          {item?.snippet?.description}
-                        </p>
+          <section className="vid-info-left-secton">
+            {videoList && videoList.length > 0 ? (
+              videoList.map((item) => {
+                const vidThumbnail = item?.snippet?.thumbnails?.default?.url;
+                // console.log(vidThumbnail);
+                return (
+                  <div key={item?.id} className="vid-info">
+                    <div className="thumbnail">
+                      <img
+                        src={vidThumbnail}
+                        alt="thumbnail"
+                        style={{
+                          height: `${item?.snippet?.thumbnails?.default?.height}px`,
+                          width: `${item?.snippet?.thumbnails?.default?.width}px`,
+                        }}
+                      />
+                      <div className="title-descript">
+                        <a
+                          target="_blank"
+                          rel="noreferrer"
+                          className="vid-title"
+                          href={`https://youtu.be/${item?.contentDetails?.videoId}`}
+                        >
+                          <p>{item?.snippet?.title}</p>
+                        </a>
+                        {item?.snippet?.description !== "" ? (
+                          <p className="description">
+                            {item?.snippet?.description}
+                          </p>
+                        ) : (
+                          <p className="description">No Description</p>
+                        )}
+                      </div>
+                    </div>
+                  </div>
+                );
+              })
+            ) : (
+              <p>Loading...</p>
+            )}
+          </section>
+          <section className="vid-info-section-right">
+            {videoList && videoList.length > 0 ? (
+              videoList.map((item, index) => {
+                return (
+                  <div className="right-section-info" key={item?.id}>
+                    <div className="status-div">
+                      <p className="status">{item?.status?.privacyStatus}</p>
+                    </div>
+                    <div className="publishedAt-div">
+                      <p className="publishedAt">{formattedDate[index]}</p>
+                    </div>
+                    <div className="view-count-div">
+                      {item?.status?.privacyStatus === "private" ? (
+                        <p className="view-count">0</p>
                       ) : (
-                        <p className="description">No Description</p>
+                        <p>view</p>
                       )}
                     </div>
                   </div>
-                </div>
-              );
-            })
-          ) : (
-            <p>No videos available</p>
-          )}
+                );
+              })
+            ) : (
+              <p>Loading...</p>
+            )}
+          </section>
         </div>
       </div>
     </div>
--- src/components/context/UserContextProvider.jsx
+++ src/components/context/UserContextProvider.jsx
@@ -7,6 +7,8 @@ const UserContextProvider = ({ children }) => {
   const [cookie, setCookie] = useState("");
   const [playlist, setPlayList] = useState("");
   const [videoList, setVideoList] = useState();
+  const [videoId, setVideoId] = useState([]);
+  const [viewCount, setViewCount] = useState();
 
   useEffect(() => {
     const cookieValue = document.cookie;
@@ -27,7 +29,7 @@ const UserContextProvider = ({ children }) => {
       const fetchData = async () => {
         if (cookie) {
           const response = await fetch(
-            `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics&mine=true&key=${
+            `https://youtube.googleapis.com/youtube/v3/channels?part=snippet%2CcontentDetails%2Cstatistics%2Cstatus&mine=true&key=${
               import.meta.env.VITE_API_KEY
             }`,
             {
@@ -55,7 +57,9 @@ const UserContextProvider = ({ children }) => {
     const fetchPlaylist = async () => {
       if (result && cookie && playlist) {
         const response2 = await fetch(
-          `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails&maxResults=25&playlistId=${playlist}&key=AIzaSyDALZrN9aPelGVgnPr1n9bJ1r_oAyGjbQ0`,
+          `https://youtube.googleapis.com/youtube/v3/playlistItems?part=snippet%2CcontentDetails%2Cid%2Cstatus&maxResults=25&playlistId=${playlist}&key=${
+            import.meta.env.VITE_API_KEY
+          }`,
           {
             headers: {
               Authorization: `Bearer ${cookie}`,
@@ -71,10 +75,39 @@ const UserContextProvider = ({ children }) => {
     fetchPlaylist();
   }, [playlist, cookie, result]);
 
-  // useEffect(() => {
-  //   if (videoList) console.log(videoList);
-  // }, [videoList]);
+  useEffect(() => {
+    if (videoList && videoList.length > 0) {
+      setVideoId(
+        videoList &&
+          videoList.length > 0 &&
+          videoList.map((item) => {
+            return item?.contentDetails?.videoId;
+          })
+      );
+    }
+  }, [videoList]);
 
+  const getData = async () => {
+    const response = await fetch(
+      `https://youtube.googleapis.com/youtube/v3/videos?part=statistics&id=Ks-_Mh1QhMc&key=${
+        import.meta.env.VITE_API_KEY
+      }`,
+      {
+        headers: {
+          Authorization: `Bearer ${cookie}`,
+          Accept: "application/json",
+        },
+      }
+    );
+    const data = await response.json();
+    setViewCount(data);
+  };
+
+  useEffect(() => {
+    if (viewCount && viewCount.length > 0) {
+      console.log(viewCount);
+    }
+  }, [viewCount]);
   return (
     <UserContext.Provider value={{ result, cookie, videoList }}>
       {children}
