863f84c7926f3d66b7ced7124b96bb8937ccd3c6
tejas0105 <tspathak0105@gmail.com>
2024-01-19 18:18:31 +0530

    UPDATED

--- src/components/Comments.jsx
+++ src/components/Comments.jsx
@@ -1,12 +1,15 @@
 import { useContext, useEffect } from "react";
 import CommentContext from "./context/CommentContext";
+import UserContext from "./context/UserContext";
 
 const Comments = () => {
   const { comments } = useContext(CommentContext);
+  const { videoList } = useContext(UserContext);
   // console.log(comments);
   useEffect(() => {
-    if (comments && comments.length > 0) console.log(comments);
-  }, [comments]);
+    if (comments && comments.length > 0 && videoList && videoList.length > 0)
+      console.log(videoList);
+  }, [comments, videoList]);
 
   return (
     <div className="comments-div overflow-auto h-[calc(100vh-4rem)]">
@@ -17,8 +20,15 @@ const Comments = () => {
       </div>
       {comments &&
         comments.length > 0 &&
+        videoList &&
+        videoList.length > 0 &&
         comments.map((item, index) => {
-          return <div key={index} className="border h-12 mt-12"></div>;
+          if (
+            item?.snippet?.topLevelComment?.snippet?.videoId ===
+            videoList[index]?.contentDetails?.videoId
+          ) {
+            return <div key={index} className="border h-12 mt-12"></div>;
+          }
         })}
     </div>
   );
